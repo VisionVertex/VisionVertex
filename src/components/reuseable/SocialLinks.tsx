0b85310d3925d7438f3908c95d23981620662d8a
Mohammad Iqbal <63330022+Iqbal0786@users.noreply.github.com>
2023-08-20 18:10:08 +0530

    Update SocialLinks.tsx

--- src/components/reuseable/SocialLinks.tsx
+++ src/components/reuseable/SocialLinks.tsx
@@ -7,9 +7,9 @@ type SocialLinksProps = { className?: string };
 const links = [
   { id: 1, icon: 'uil uil-twitter', url: 'https://twitter.com/uilibofficial' },
   { id: 2, icon: 'uil uil-facebook-f', url: 'https://facebook.com/uiLibOfficial/' },
-  { id: 3, icon: 'uil uil-dribbble', url: '#' },
+  { id: 3, icon: 'uil uil-linkedin', url: '#' },
   { id: 4, icon: 'uil uil-instagram', url: 'https://www.instagram.com/uilibofficial/' },
-  { id: 5, icon: 'uil uil-youtube', url: 'https://www.youtube.com/channel/UCsIyD-TSO1wQFz-n2Y4i3Rg' }
+  // { id: 5, icon: 'uil uil-youtube', url: 'https://www.youtube.com/channel/UCsIyD-TSO1wQFz-n2Y4i3Rg' }
 ];
 
 const SocialLinks: FC<SocialLinksProps> = ({ className = 'nav social social-white mt-4' }) => {
