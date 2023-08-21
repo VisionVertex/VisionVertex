ca7c2bd736fcc71ecee18c6a4e65073705d32ecb
saurabh yadav <72351102+saurabhyadav01@users.noreply.github.com>
2023-08-21 17:40:50 +0530

    remove icon

--- src/components/blocks/navbar/partials/Social.tsx
+++ src/components/blocks/navbar/partials/Social.tsx
@@ -1,24 +1,30 @@
+import React from 'react';
+
+
 const Social = () => {
   return (
-    <li className="nav-item">
-      <nav className="nav social social justify-content-end text-end">
+    <div className="social-container"> {/* Wrap in a container */}
+      <li className="nav-item">
+        <nav className="nav social social justify-content-end text-end">
         <a href="#">
-          <i className="uil uil-twitter" />
-        </a>
+            <i className="uil uil-linkedin" />
+          </a>
+          <a href="#">
+            <i className="uil uil-twitter" />
+          </a>
 
-        <a href="#">
-          <i className="uil uil-facebook-f" />
-        </a>
+          <a href="#">
+            <i className="uil uil-facebook-f" />
+          </a>
 
-        <a href="#">
-          <i className="uil uil-linkedin" />
-        </a>
+        
 
-        <a href="#">
-          <i className="uil uil-instagram" />
-        </a>
-      </nav>
-    </li>
+          {/* <a href="#">
+            <i className="uil uil-instagram" />
+          </a> */}
+        </nav>
+      </li>
+    </div>
   );
 };
 
